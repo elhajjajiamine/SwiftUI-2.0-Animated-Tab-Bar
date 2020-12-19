d66b10cd82023721d3c76a80cd4980ad9b7f925f
elhajjajiamine <aminselhajjaji@gmail.com>
2020-12-19 11:07:50 +0100

    Initial Commit

---
+++ SwiftUI 2.0 Animated Tab Bar/SwiftUI_2_0_Animated_Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_2_0_Animated_Tab_BarApp.swift
+//  SwiftUI 2.0 Animated Tab Bar
+//
+//  Created by elhajjaji on 18/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_2_0_Animated_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
